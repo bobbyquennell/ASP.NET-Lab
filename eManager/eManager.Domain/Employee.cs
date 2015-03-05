211167b330bb34a2b51d601e67c1bb71b8f51eaf
Bobby Wang <bobbyquennell@gmail.com>
2015-03-05 17:38:27 +1100

    delete default class file, add an new class to describe Employee: Employee.cs

--- eManager/eManager.Domain/Employee.cs
+++ eManager/eManager.Domain/Employee.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace eManager.Domain
 {
-    public class Class1
+    public class Employee
     {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
     }
 }
