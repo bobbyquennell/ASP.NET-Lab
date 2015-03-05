8dcbad619a3fd13f94b3199b4399640b84ef7bbd
Bobby Wang <bobbyquennell@gmail.com>
2015-03-05 17:50:29 +1100

    create an abstract interface to employee and department

---
+++ eManager/eManager.Domain/IDepartmentDataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eManager.Domain
+{
+    public interface IDepartmentDataSource
+    {
+        public IQueryable<Employee> Employees { get;}
+        public IQueryable<Department> Departments { get;}
+    }
+}
