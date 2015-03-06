8a403df27ec8d2f7dc95c15227493f44c8b24a96
Bobby Wang <bobbyquennell@gmail.com>
2015-03-06 11:16:50 +1100

    add nullable DateTime property HireDate in Employee.cs

--- eManager/eManager.Domain/Employee.cs
+++ eManager/eManager.Domain/Employee.cs
@@ -10,5 +10,6 @@ namespace eManager.Domain
     {
         public virtual int EmployeeId { get; set; }
         public virtual string Name { get; set; }
+        public virtual DateTime? HireDate { get; set; }
     }
 }
