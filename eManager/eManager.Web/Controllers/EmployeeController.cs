9fb9f4b78df0494b6ff83bb4cf8d8fc943edb054
Bobby Wang <bobbyquennell@gmail.com>
2015-03-06 11:08:41 +1100

    add a actionLink in the Detail view, create a empty Employee controller

---
+++ eManager/eManager.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eManager.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
