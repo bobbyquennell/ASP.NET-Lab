dc933439fd0297c605714b3d40934fff563536c7
Bobby Wang <bobbyquennell@gmail.com>
2015-03-10 09:58:59 +1100

    create a new project for WebAPI test

---
+++ video/video/Controllers/VideosController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace video.Controllers
+{
+    public class VideosController : ApiController
+    {
+
+        // GET api/video
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/video/5
+        public string Get(int id)
+        {
+            return "value" + " " + id.ToString();
+        }
+
+        // POST api/video
+        public Video Post(Video video)
+        {
+            return video;
+        }
+
+        // PUT api/video/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/video/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
