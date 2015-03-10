52c1bc59c04ff0a95b01d9fc440926a8b74b22fe
Bobby Wang <bobbyquennell@gmail.com>
2015-03-11 09:24:41 +1100

    using HttpClient in an console project to get and parse data via WEB API

--- video/ConsoleClient/Program.cs
+++ video/ConsoleClient/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleClient
 {
@@ -10,7 +14,22 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
-            
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/xml"));
+
+            var result = client.GetAsync(new Uri("http://localhost:1719/api/videos")).Result;
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var doc = XDocument.Load(result.Content.ReadAsStreamAsync().Result);
+                var ns = (XNamespace) "http://schemas.datacontract.org/2004/07/Videos.Models";
+                foreach (var title in doc.Descendants(ns + "Title"))
+                {
+                    Console.WriteLine(title.Value);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
