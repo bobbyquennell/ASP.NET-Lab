57fa7ee72a97c130167095285f2e545c75085b82
Bobby Wang <bobbyquennell@gmail.com>
2015-03-11 11:28:28 +1100

    add a simple slow operation and main process work in the same thread.

--- TaskParallelLibraryTest/TaskParallelLibraryTest/Program.cs
+++ TaskParallelLibraryTest/TaskParallelLibraryTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskParallelLibraryTest
@@ -10,6 +11,22 @@ namespace TaskParallelLibraryTest
     {
         static void Main(string[] args)
         {
+            var result = SlowOperation();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Slow operation result:{0}", result);
+            Console.WriteLine("Main complete on {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.ReadKey();
+        }
+
+        private static object SlowOperation()
+        {
+            Console.WriteLine("Slow operation started on {0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("Slow operation complete on {0}",Thread.CurrentThread.ManagedThreadId);
+            return 42;
         }
     }
 }
