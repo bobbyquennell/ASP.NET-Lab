e38b4a649b3ec791aa8fda53d2a3334f9bcac6d6
Bobby Wang <bobbyquennell@gmail.com>
2015-03-11 15:12:48 +1100

    MVC4 and Mobile Development:    DisplayMode Provider.   this is what you can do with display modes. You can target your output to specific browsers

---
+++ MvcNormal/MvcNormal/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace MvcNormal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult DisplayModes()
+        {
+            var model = DisplayModeProvider.Instance.Modes;
+            return View(model);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcNormal/MvcNormal/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.WebPages;
+
+namespace MvcNormal
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            AddDisplayModes();
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+        }
+
+        private void AddDisplayModes()
+        {
+            DisplayModeProvider.Instance.Modes.Insert(0,
+                new DefaultDisplayMode("Mobile")
+                {
+                    ContextCondition = ctx => ctx.Request.UserAgent.Contains("iPad")
+                });
+            DisplayModeProvider.Instance.Modes.Insert(0,
+                new DefaultDisplayMode("Silk")
+                {
+                    ContextCondition = c => c.Request.UserAgent.Contains("Silk/")
+                });
+        }
+    }
+}
