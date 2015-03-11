81e4769995983f5962427b8b07f07875baeff21a
Bobby Wang <bobbyquennell@gmail.com>
2015-03-12 08:34:25 +1100

    MVC5 Katana test project

---
+++ KatanaIntro/KatanaIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace KatanaIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping!");
+            }
+
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx =>
+            {
+                return ctx.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
