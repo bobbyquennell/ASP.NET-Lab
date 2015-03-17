1cb774e985f08a8e4f3221b6004a5027f55a1f4e
Bobby Wang <bobbyquennell@gmail.com>
2015-03-17 11:26:12 +1100

    MVC5 Fundamentals 6.Entity Framework 6 6.2 Starting Up

---
+++ Books/Books.Web/DataContext/BooksDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Books.Entities;
+
+namespace Books.Web.DataContext
+{
+    public class BooksDb:DbContext
+    {
+        public BooksDb()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Books/Books.Web/DataContext/IdentityDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Books.Web.Models;
+
+namespace Books.Web.DataContext
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    }
+}
