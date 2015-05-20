1b15fa066346193a3d776d8100f78038253659df
Bobby Wang <bobbyquennell@gmail.com>
2015-05-20 22:42:53 +1000

    try different Action Result from a controller--Pluralsight Building Apps with MVC4 2.4 Content RedirectPermanent RedirectToRoute RedirectToAction File Json

--- OdeToFoodExercise/OdeToFoodExercise/Controllers/CuisineController.cs
+++ OdeToFoodExercise/OdeToFoodExercise/Controllers/CuisineController.cs
@@ -18,7 +18,12 @@ namespace OdeToFoodExercise.Controllers
             //var message = String.Format("the parameter is: {0}", name);
             //return View();
             var encodedMessage = Server.HtmlEncode(name);
-            return Content(encodedMessage);
+            //return Content(encodedMessage);
+           // return RedirectPermanent("http://www.sina.com.cn");
+            //return RedirectToRoute("Default", new { controller = "Home", action = "About" });
+            //return RedirectToAction("About", "Home" ,new { name = name});
+            //return File(Server.MapPath("~/Content/site.css"), "text/css");
+            return Json(new { Message = encodedMessage, Name = "Bobby" }, JsonRequestBehavior.AllowGet);
         }
 
     }
