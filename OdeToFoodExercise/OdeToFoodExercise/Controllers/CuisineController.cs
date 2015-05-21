a486c7be03d9211d789dab3c0ba50ad2daf16fb9
Bobby Wang <bobbyquennell@gmail.com>
2015-05-21 14:47:34 +1000

    Action Filters; Handle Error, Authorized, build your own custom Filter: Log Attribute

--- OdeToFoodExercise/OdeToFoodExercise/Controllers/CuisineController.cs
+++ OdeToFoodExercise/OdeToFoodExercise/Controllers/CuisineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFoodExercise.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace OdeToFoodExercise.Controllers
 {
+    [Log]// using your own action filter: Log
     public class CuisineController : Controller
     {
         //
@@ -28,7 +30,7 @@ namespace OdeToFoodExercise.Controllers
         [HttpGet]
         public ActionResult Search()
         {
-
+            throw new Exception("Something terrible is happening!");
             return Content("Search!");
 
         }
---
+++ OdeToFoodExercise/OdeToFoodExercise/Filters/LogAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFoodExercise.Filters // build your own custom action filter: Log
+{
+    public class LogAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //we can check and print Executing Context here
+            base.OnActionExecuting(filterContext);
+        }
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
