127cb26fb1c6f0104eeb823c090cc1ba78d29fc7
Bobby Wang <bobbyquennell@gmail.com>
2015-05-21 12:04:55 +1000

    2.5 Action Selectors [HttpGet] [HttpPost] [ActionName("Modify")]

--- OdeToFoodExercise/OdeToFoodExercise/Controllers/CuisineController.cs
+++ OdeToFoodExercise/OdeToFoodExercise/Controllers/CuisineController.cs
@@ -10,7 +10,7 @@ namespace OdeToFoodExercise.Controllers
     {
         //
         // GET: /Cuisine/
-
+        [HttpPost]
         public ActionResult Search(string name = "French")
         {   
              
@@ -18,12 +18,19 @@ namespace OdeToFoodExercise.Controllers
             //var message = String.Format("the parameter is: {0}", name);
             //return View();
             var encodedMessage = Server.HtmlEncode(name);
-            //return Content(encodedMessage);
+            return Content(encodedMessage);
            // return RedirectPermanent("http://www.sina.com.cn");
             //return RedirectToRoute("Default", new { controller = "Home", action = "About" });
             //return RedirectToAction("About", "Home" ,new { name = name});
             //return File(Server.MapPath("~/Content/site.css"), "text/css");
-            return Json(new { Message = encodedMessage, Name = "Bobby" }, JsonRequestBehavior.AllowGet);
+            //return Json(new { Message = encodedMessage, Name = "Bobby" }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult Search()
+        {
+
+            return Content("Search!");
+
         }
 
     }
