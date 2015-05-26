cb398553a1bf3206eb0882532bb18d21528b0f23
Bobby Wang <bobbyquennell@gmail.com>
2015-05-26 11:16:34 +1000

    4.3 Building Entities--- Before using  the DataBase, we should instantiate the DbContext firstly, and also override the "DIspose" method to clear the resource when don't need

--- OdeToFoodExercise/OdeToFoodExercise/Controllers/HomeController.cs
+++ OdeToFoodExercise/OdeToFoodExercise/Controllers/HomeController.cs
@@ -9,6 +9,10 @@ namespace OdeToFoodExercise.Controllers
 {
     public class HomeController : Controller
     {
+        //in order to show how to use the EF, suppose we want to show  restaurants from the database
+        //in the home page.
+           //First, before using the database, we instanciate the OdeToFoodDb firstly.
+        OdeToFoodDb _db = new OdeToFoodDb();
         public ActionResult Index()
         {
             var controller = RouteData.Values["controller"];
@@ -16,6 +20,7 @@ namespace OdeToFoodExercise.Controllers
             var id = RouteData.Values["id"];
             var message = string.Format("route data is : {0}/{1}/{2}", controller, action, id);
             ViewBag.Message = message;
+            
 
             return View();
         }
@@ -36,6 +41,15 @@ namespace OdeToFoodExercise.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        //second, this is a diposable resource, we should override Dispose method here
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
