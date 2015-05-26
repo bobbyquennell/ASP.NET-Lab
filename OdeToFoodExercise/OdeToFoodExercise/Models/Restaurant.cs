7b6b32050de8e2020d0c33f939be0f537252c6a8
Bobby Wang <bobbyquennell@gmail.com>
2015-05-26 10:43:44 +1000

    4.3 Building Entities----the former RestaurantReview class combined too many stuff together, the restaurant name, city, country and reviews. we separate these in two classes and form a relationship between them.

---
+++ OdeToFoodExercise/OdeToFoodExercise/Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdeToFoodExercise.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        //any restaurant can have multiple associated reviews, we can put that in 
+        //the ICollection:
+        public ICollection<RestaurantReview> Reviews { get; set; }
+
+    }
+}
--- OdeToFoodExercise/OdeToFoodExercise/Models/RestaurantReview.cs
+++ OdeToFoodExercise/OdeToFoodExercise/Models/RestaurantReview.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Web;
 
 namespace OdeToFoodExercise.Models
-{
+{// this class combined too much stuff together, the restaurant name,the reviews
+    // we need seperate these in two diffrent classes and form a relationship between them.
     public class RestaurantReview
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
         public int Rating { get; set; }
+        public string Body { get; set; }
+        public int RestaurantId { get; set; }// add to restaurant id here will help to create
+        //the relationship between the reviews and the restaurant
+
 
     }
 }
