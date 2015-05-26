934a7b055e4ef9dbc7d9709919c92427c0f72293
Bobby Wang <bobbyquennell@gmail.com>
2015-05-26 10:57:51 +1000

    4.3 Building Entities-- we also need to add a class for EF to retrieve our data,     //and this class need to derived from a EF class named--DbContext

---
+++ OdeToFoodExercise/OdeToFoodExercise/Models/OdeToFoodDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OdeToFoodExercise.Models
+{
+    // we also need to add this class for EF to retrieve our data,
+    //and this class need to derived from a EF class named--DbContext
+    public class OdeToFoodDb:DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantReview> Reviews { get; set; }
+    }
+}
