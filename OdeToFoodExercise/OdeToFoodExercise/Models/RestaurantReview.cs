436882dfd9f0cc1627d3edef02e3f7cd4456eac1
Bobby Wang <bobbyquennell@gmail.com>
2015-05-29 14:47:04 +1000

    5.8 Custom Validation-----second approach of custom validation----IValidatableObject

--- OdeToFoodExercise/OdeToFoodExercise/Models/RestaurantReview.cs
+++ OdeToFoodExercise/OdeToFoodExercise/Models/RestaurantReview.cs
@@ -7,31 +7,32 @@ using System.Web;
 namespace OdeToFoodExercise.Models
 {// this class combined too much stuff together, the restaurant name,the reviews
     // we need seperate these in two diffrent classes and form a relationship between them.
-    public class MaxWordsAttribute : ValidationAttribute
-    {
-        public MaxWordsAttribute(int maxWords)
-            : base("{0} has too many words.")
-        {
-            _maxWords = maxWords;
-        }
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-                var valueString = value.ToString();
-                if(valueString.Split(' ').Length > _maxWords)
-                {
-                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
-                    return new ValidationResult(errorMessage);
-                }
-            }
-            return ValidationResult.Success;
-        }
+
+    //public class MaxWordsAttribute : ValidationAttribute
+    //{
+    //    public MaxWordsAttribute(int maxWords)
+    //        : base("{0} has too many words.")
+    //    {
+    //        _maxWords = maxWords;
+    //    }
+    //    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    //    {
+    //        if (value != null)
+    //        {
+    //            var valueString = value.ToString();
+    //            if(valueString.Split(' ').Length > _maxWords)
+    //            {
+    //                var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+    //                return new ValidationResult(errorMessage);
+    //            }
+    //        }
+    //        return ValidationResult.Success;
+    //    }
 
 
-        private readonly int _maxWords;
-    }
-    public class RestaurantReview
+    //    private readonly int _maxWords;
+    //}
+    public class RestaurantReview:IValidatableObject//Custom Validation way2: IValidatableObject approach
     {
         public int Id { get; set; }
         [Range(1,10, ErrorMessage="custom")]
@@ -42,11 +43,17 @@ namespace OdeToFoodExercise.Models
         public string Body { get; set; }
         [Display(Name=" User Name")]
         [DisplayFormat(NullDisplayText="anonymous")]
-        [MaxWordsAttribute(1)]// or[MaxWords(1)]
+        //[MaxWordsAttribute(1)]// or[MaxWords(1)]
         public string Reviewer { get; set; }
         public int RestaurantId { get; set; }// add to restaurant id here will help to create
         //the relationship between the reviews and the restaurant
 
 
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Rating < 2 && Reviewer.ToLower().Contains("fuck"))
+            {yield return new ValidationResult("You cannot do this");}
+        }
     }
 }
