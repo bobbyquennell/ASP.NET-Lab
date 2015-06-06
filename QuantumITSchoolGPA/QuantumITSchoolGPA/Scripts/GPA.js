0a5707b8de66d19d02fb2e77d3c23af6e1cacc34
Bobby Wang <bobbyquennell@gmail.com>
2015-06-06 11:42:31 +1000

    code test ----Highlight the student's name if his/her GPA is equal  or higher than 3.2

--- QuantumITSchoolGPA/QuantumITSchoolGPA/Scripts/GPA.js
+++ QuantumITSchoolGPA/QuantumITSchoolGPA/Scripts/GPA.js
@@ -7,10 +7,18 @@
         };
         $.ajax(options).done(function (data) {
             $("#StudentList").html(data);
+            $("td.studentGpa").each(function (index) {
+                if (parseFloat($(this).text()) >= 3.2) {
+                    $(this).prev().prev().css('color', 'red');
+                    console.log(index + "found High GPA!" + $(this).text());
+                }
+                
+            });
         });
         return false;
     };
 
     $(".main-content").on("click", ".CourseName a", ajaxGetStudentList);
+
 
 });
