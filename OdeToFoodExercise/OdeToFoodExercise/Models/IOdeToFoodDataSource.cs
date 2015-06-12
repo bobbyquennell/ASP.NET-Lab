fb82e438d43d64fc4a5aea838b7781b5f12f4b47
Bobby Wang <bobbyquennell@gmail.com>
2015-06-12 16:54:52 +1000

    9.4 Home Controller Tests----To isolate the Database from the Home Controller , step 1: define a interface which will represent all the operation we can do against the Database, let the OdeToFoodDb class implement this interface

---
+++ OdeToFoodExercise/OdeToFoodExercise/Models/IOdeToFoodDataSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFoodExercise.Models
+{
+    public interface IOdeToFoodDataSource: IDisposable
+    {
+        IQueryable<T> Query<T>() where T: class;
+    }
+}
--- OdeToFoodExercise/OdeToFoodExercise/Models/OdeToFoodDb.cs
+++ OdeToFoodExercise/OdeToFoodExercise/Models/OdeToFoodDb.cs
@@ -8,11 +8,16 @@ namespace OdeToFoodExercise.Models
 {
     // we also need to add this class for EF to retrieve our data,
     //and this class need to derived from a EF class named--DbContext
-    public class OdeToFoodDb:DbContext
+    public class OdeToFoodDb:DbContext, IOdeToFoodDataSource
     {
         public OdeToFoodDb() : base("name=OdeTOFoodDb") { }
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<RestaurantReview> Reviews { get; set; }
         public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public IQueryable<T> Query<T>() where T : class
+        {
+            return Set<T>();//The Set<T>() method is derived from base class: DbContext
+        }
     }
 }
