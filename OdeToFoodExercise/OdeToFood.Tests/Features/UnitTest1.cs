a43c59d521e67a3c98a8cfc49bc23f5d7c95bb10
Bobby Wang <bobbyquennell@gmail.com>
2015-06-12 11:44:39 +1000

    9.3 Test Driven Design-----TDD Cycle Step 1: RED: add a new test for the requirement : should only take the first N reviews to average the overall rating. the test compiles but fails because we didn't  develop this feature yet

--- OdeToFoodExercise/OdeToFood.Tests/Features/UnitTest1.cs
+++ OdeToFoodExercise/OdeToFood.Tests/Features/UnitTest1.cs
@@ -64,7 +64,17 @@ namespace OdeToFood.Tests.Features
 
             Assert.AreEqual(1, result.Rating);
         }
+        [TestMethod]
+        public void Should_Only_Rate_From_Firt_N_Reviews()
+        {
+            var model = BuildRestaurantAndReviews(new int[] { 1, 1, 1, 1, 3, 3, 3, 3 });
+            var alg = new SimpleAverageRatingAlgorithm();
+            var rater = new RestaurantRater(model, alg);
+            RatingResult result = rater.Compute(4);
 
+            Assert.AreEqual(1, result.Rating);
+
+        }
         private Restaurant BuildRestaurantAndReviews(params int[] ratings)
         {
             Restaurant rest = new Restaurant();
