c3a0aa164a529efc4f11ee896474546a972d2e63
bobby wang <bobbyquennell@gmail.com>
2015-06-16 13:56:54 +1000

    .Net Testing with NUnit--------------2.3 Asserting Numerical  Equality: add Calculator for assert numerical equality test.

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int AddInts(int a, int b)
+        {
+            return a + b;
+        }
+        public double AddDoubles(double a, double b)
+        {
+            return a + b;
+        }
+        public int Divide(int value, int by)
+        {
+            if (value > 100)
+            {
+                throw new ArgumentOutOfRangeException("by"); //bug for demo purposes 
+            }
+            return value / by;
+        }
+    }
+}
