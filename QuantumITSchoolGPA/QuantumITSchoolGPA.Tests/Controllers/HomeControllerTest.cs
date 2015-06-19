b52f18c6029d4f937826cc54e378223fb32dea87
bobby wang <bobbyquennell@gmail.com>
2015-06-19 16:19:14 +1000

    CodeTest----Refactor HomeControllerTest with [Setup] attribute.

--- QuantumITSchoolGPA/QuantumITSchoolGPA.Tests/Controllers/HomeControllerTest.cs
+++ QuantumITSchoolGPA/QuantumITSchoolGPA.Tests/Controllers/HomeControllerTest.cs
@@ -15,16 +15,14 @@ namespace QuantumITSchoolGPA.Tests.Controllers
     [TestFixture]
     public class HomeControllerTest
     {
+        private HomeController sut;
+        private FakeSchoolGpaDb db;
         [Test]
         public void Index_Should_Return_Class_Model_List()
         {
-            // Arrange
-            var db = new FakeSchoolGpaDb();
-            db.AddSet(TestData.Classes);
-            HomeController controller = new HomeController(db);
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            ViewResult result = sut.Index() as ViewResult;
             IEnumerable<Class> model = result.Model as IEnumerable<Class>;
             // Assert
             Assert.That(model.Count(), Is.EqualTo(4));
@@ -33,18 +31,23 @@ namespace QuantumITSchoolGPA.Tests.Controllers
         [Test]
         public void ShowStudentList_Should_Return_a_ClassModel_with_Student_List()
         {
-            // Arrange
-            var db = new FakeSchoolGpaDb();
-            db.AddSet(TestData.Classes);
-            HomeController controller = new HomeController(db);
+            //// Arrange
             int classId = 1;
 
             // Act
-            PartialViewResult result = controller.ShowStudentList(classId) as PartialViewResult;
+            PartialViewResult result = sut.ShowStudentList(classId) as PartialViewResult;
             Class model = result.Model as Class;
             // Assert
             Assert.That(model.Id, Is.EqualTo(classId));
             Assert.That(model.Students.Count, Is.EqualTo(1));
+        }
+
+        [SetUp]
+        public void Arrange()
+        {
+             db = new FakeSchoolGpaDb();
+            db.AddSet(TestData.Classes);
+            sut = new HomeController(db);
         }
 
        
