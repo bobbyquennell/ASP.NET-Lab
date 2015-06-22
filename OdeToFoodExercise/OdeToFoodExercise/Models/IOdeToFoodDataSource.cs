6f5b8d9b51294cdf60410eaf7e230d0b842e705f
bobby wang <bobbyquennell@gmail.com>
2015-06-22 22:21:49 +1000

    OdeToFood Exercise-----add some comments

--- OdeToFoodExercise/OdeToFoodExercise/Models/IOdeToFoodDataSource.cs
+++ OdeToFoodExercise/OdeToFoodExercise/Models/IOdeToFoodDataSource.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace OdeToFoodExercise.Models
 {
     public interface IOdeToFoodDataSource: IDisposable
-    {
+    {//this is kand of IRepoistory pattern
         IQueryable<T> Query<T>() where T: class;
         void Add<T>(T entity) where T : class;
         void Update<T>(T entity) where T : class;
