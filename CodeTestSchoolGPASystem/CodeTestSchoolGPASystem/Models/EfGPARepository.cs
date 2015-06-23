28e92868ac48260b4f40bff1d6056f2b80e182dd
bobby wang <bobbyquennell@gmail.com>
2015-06-23 14:28:53 +1000

    MVCKata---add EfGPARepository in GPA.Web project.

---
+++ CodeTestSchoolGPASystem/CodeTestSchoolGPASystem/Models/EfGPARepository.cs
@@ -0,0 +1,42 @@
+﻿using GPA.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using GPA.Domain;
+
+namespace GPASystem.Web.Models
+{
+    public class EfGPARepository:DbContext, IRepository
+    {
+        public EfGPARepository() : base("name = EfGPARepository") { }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public IQueryable<TEntity> GetAll<TEntity>() where TEntity : class
+        {
+            return Set<TEntity>();
+        }
+
+        public void Add<TEntity>(TEntity entity) where TEntity : class
+        {
+           Set<TEntity>().Add(entity);
+        }
+
+        public void Update<TEntity>(TEntity entity) where TEntity : class
+        {
+            Entry(entity).State = System.Data.EntityState.Modified;
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            Set<TEntity>().Remove(entity);
+        }
+
+        public new void SaveChanges()
+        {
+            SaveChanges();
+        }
+    }
+}
--- CodeTestSchoolGPASystem/GPA.Domain/Entities/Student.cs
+++ CodeTestSchoolGPASystem/GPA.Domain/Entities/Student.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GPA.Domain
 {
-    class Student
+    public class Student
     {
         public int Id { get; set; }
         public string Name { get; set; }
