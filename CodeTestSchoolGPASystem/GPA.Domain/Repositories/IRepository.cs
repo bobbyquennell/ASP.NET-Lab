fe8208ff02e2e62605b95fee140ead5a190865d4
bobby wang <bobbyquennell@gmail.com>
2015-06-23 13:39:05 +1000

    MVCKata-----add IRepository in domain.

---
+++ CodeTestSchoolGPASystem/GPA.Domain/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPA.Domain.Repositories
+{
+    public interface IRepository:IDisposable
+    {
+        IQueryable<TEntity> GetAll<TEntity>() where TEntity : class;
+        void Add<TEntity>(TEntity entity) where TEntity : class;
+        void Update<TEntity>(TEntity entity) where TEntity : class;
+        void Delete<TEntity>(TEntity entity) where TEntity : class;
+        void SaveChanges();
+
+    }
+}
