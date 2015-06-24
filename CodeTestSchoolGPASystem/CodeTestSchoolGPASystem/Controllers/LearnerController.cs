a98063df467522ecd9bb3574cd2f7cbbb40a6a55
bobby wang <bobbyquennell@gmail.com>
2015-06-24 16:04:50 +1000

    MVCKata------add a action link on the course name, which should trigger a ajax request and show the student list of that course in the same page.

---
+++ CodeTestSchoolGPASystem/CodeTestSchoolGPASystem/Controllers/LearnerController.cs
@@ -0,0 +1,110 @@
+﻿using GPA.Domain.Entities;
+using GPASystem.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GPASystem.Web.Controllers
+{
+    public class LearnerController : Controller
+    {
+        EfGPARepository _GpaRepo = new EfGPARepository();
+        //
+        // GET: /Learner/
+
+        public ActionResult Index(int id)
+        {
+            var model = _GpaRepo.GetAll<Course>().Single(c => c.Id == id);
+
+            return View();
+        }
+
+        //
+        // GET: /Learner/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Learner/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Learner/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Learner/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Learner/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Learner/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Learner/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
