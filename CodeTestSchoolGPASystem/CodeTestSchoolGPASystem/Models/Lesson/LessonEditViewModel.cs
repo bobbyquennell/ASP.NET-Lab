3f850f98127f9c08abce277c2c046278a58c33e6
bobby wang <bobbyquennell@gmail.com>
2015-06-25 15:48:11 +1000

    MVCKata----Add some basic [Required] model validation in view model.

--- CodeTestSchoolGPASystem/CodeTestSchoolGPASystem/Models/Lesson/LessonEditViewModel.cs
+++ CodeTestSchoolGPASystem/CodeTestSchoolGPASystem/Models/Lesson/LessonEditViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace GPASystem.Web.Models.Lesson
 {
     public class LessonEditViewModel
     {
+        [Required]
         public string CourseName { get; set; }
+        [Required]
         public string Location { get; set; }
+        [Required]
         public string TeacherName { get; set; }
     }
 }
