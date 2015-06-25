bb670b963e6670c62d764c69f66a7afe2a600196
bobby wang <bobbyquennell@gmail.com>
2015-06-25 22:00:57 +1000

    MVCKata----Using Mock.Setup method to mock IRepository.GetAll<Course>() methods. modified the test case name and the assertion. This is a real test case that mock LINQ!!!!!!

--- CodeTestSchoolGPASystem/CodeTestSchoolGPASystem.Tests/Controllers/HomeControllerTest.cs
+++ CodeTestSchoolGPASystem/CodeTestSchoolGPASystem.Tests/Controllers/HomeControllerTest.cs
@@ -10,6 +10,7 @@ using NUnit.Framework;
 using Moq;
 using GPA.Domain.Repositories;
 using GPA.Domain.Entities;
+using GPASystem.Web.Models.Home;
 
 namespace GPASystem.Web.Tests.Controllers
 {
@@ -17,17 +18,22 @@ namespace GPASystem.Web.Tests.Controllers
     public class HomeControllerTest
     {
         [Test]
-        public void Index()
+        public void Index_Should_Return_Course_List()
         {
             // Arrange
             Mock<IRepository> GpaRepoMock = new Mock<IRepository>();
+            GpaRepoMock.Setup(r => r.GetAll<Course>()).Returns(new Course[]{
+             new Course{Location = "building1 Room201", Name="English"},
+             new Course{Location = "building5 Room301", Name="French"},
+             }.AsQueryable());
+
             HomeController controller = new HomeController(GpaRepoMock.Object);
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
-
+            HomeIndexViewModel model = result.Model as HomeIndexViewModel;
             // Assert
-            Assert.That(result, Is.Not.Null);
+            Assert.That(model.Courses.Count, Is.EqualTo(2));
         }
 
         //[TestMethod]
