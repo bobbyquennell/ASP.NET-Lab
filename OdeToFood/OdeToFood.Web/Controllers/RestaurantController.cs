6c45fef93610c23a2176a5c052a1bb03a6e5dbde
bobby wang <bobbyquennell@gmail.com>
2015-07-01 12:06:12 +1000

    OdeToFood Kata----Now we can see all the listed restaurants with delete and edit shortcuts

---
+++ OdeToFood/OdeToFood.Web/Controllers/RestaurantController.cs
@@ -0,0 +1,102 @@
+﻿using OdeToFood.Domain.Entities;
+using OdeToFood.Domain.Repositories;
+using OdeToFood.Web.Models;
+using OdeToFood.Web.Models.Bristo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private IRepository _repo = new EfRepository();
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            var model = _repo.GetAll<Restaurant>()
+                .Select(r => new RestaurantIndexViewModel
+                    {
+                         City = r.City,
+                          Country = r.Country,
+                           Restaurant = r.Name
+                    }
+                );
+            return View(model);
+        }
+
+        // GET: Restaurant/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Restaurant/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Restaurant/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood.Web/Models/Bristo/RestaurantIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdeToFood.Web.Models.Bristo
+{
+    public class RestaurantIndexViewModel
+    {
+        public string Restaurant { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
