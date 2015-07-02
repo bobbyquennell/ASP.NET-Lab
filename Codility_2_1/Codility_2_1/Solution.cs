e4487fb06c03a200aa1cb3979a682b2d35fdce1b
bobby wang <bobbyquennell@gmail.com>
2015-07-02 16:32:46 +1000

    Codility 2_1

---
+++ Codility_2_1/Codility_2_1/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_2_1
+{
+    public class Solution
+    {
+        public int solution(int X, int[] A)
+        {
+            int PositionFilledWithLeaf = 0;
+            int TimeCanCross = -1;
+            int[] Count = new int[X];
+            for (int i = 0; i < Count.Length; i++)
+			{
+			    Count[i] = 0;
+			}
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if(Count[A[i] - 1] == 0){
+                    Count[A[i] -1] = 1;
+                    PositionFilledWithLeaf += 1;
+                }
+                if (PositionFilledWithLeaf == X)
+                {
+                    TimeCanCross = i;
+                    break;
+                }
+            }
+            return TimeCanCross;
+        }
+    }
+}
