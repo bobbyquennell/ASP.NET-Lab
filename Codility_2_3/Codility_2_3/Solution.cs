8eb19a0ff85be15bce184855f870bceded4f4e05
bobby wang <bobbyquennell@gmail.com>
2015-07-03 15:38:24 +1000

    Codility   Lesson 2_3

---
+++ Codility_2_3/Codility_2_3/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_2_3
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int N = A.Length;
+            int[] CheckArray = new int[N];
+            int minLost = 1;
+            for (int i = 0; i < N; i++)
+            {
+                CheckArray[i] = 1 + i;
+            }
+            for (int i = 0; i < N; i++)
+            {
+                
+                if ((A[i] > 0)&&(A[i]<=N))
+                {
+                    CheckArray[A[i] - 1] = 0;
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                if (CheckArray[i] > 0)
+                {
+                    minLost = CheckArray[i];
+                    break;
+                }
+                if(CheckArray[i] == 0)
+                {
+                    minLost = i + 2;
+                }
+            }
+            return minLost;
+        }
+    }
+}
---
+++ Codility_2_3/Test/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Codility_2_3;
+namespace Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            //arrange
+            var sut = new Solution();
+            int[] A = new int[]{1,3,6,4,1,2};
+            //act
+            var result = sut.solution(A);
+            //assert
+            Assert.That(result, Is.EqualTo(5));
+        }
+        [Test]
+        public void Test2()
+        {
+            //arrange
+            var sut = new Solution();
+            int[] A = new int[] { 1, 3, 6, 3, 1, 2 };
+            //act
+            var result = sut.solution(A);
+            //assert
+            Assert.That(result, Is.EqualTo(4));
+        }
+        [Test]
+        public void Test3()
+        {
+            //arrange
+            var sut = new Solution();
+            int[] A = new int[] { -2147483648, 1, 2, 3, 5, 2147483647 };
+            //act
+            var result = sut.solution(A);
+            //assert
+            Assert.That(result, Is.EqualTo(4));
+        }
+        [Test]
+        public void Test4()
+        {
+            //arrange
+            var sut = new Solution();
+            int[] A = new int[] { 1, 2, 3, 4, 5, 6 };
+            //act
+            var result = sut.solution(A);
+            //assert
+            Assert.That(result, Is.EqualTo(7));
+        }
+        [Test]
+        public void Test5()
+        {
+            //arrange
+            var sut = new Solution();
+            int[] A = new int[] { -1, -2, -3, -4, -5, -6 };
+            //act
+            var result = sut.solution(A);
+            //assert
+            Assert.That(result, Is.EqualTo(1));
+        }
+        [Test]
+        public void Test6()
+        {
+            //arrange
+            var sut = new Solution();
+            int[] A = new int[] { 1, 2, 3, 4, 5, 6,7,8 };
+            //act
+            var result = sut.solution(A);
+            //assert
+            Assert.That(result, Is.EqualTo(9));
+        }
+    }
+}
