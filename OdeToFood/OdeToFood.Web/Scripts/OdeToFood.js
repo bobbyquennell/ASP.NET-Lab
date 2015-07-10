09f10d7dfd8aa4e64b9a7f82e9b7c6b4a526cfbe
bobby wang <bobbyquennell@gmail.com>
2015-07-10 14:52:24 +1000

    OdeToFood Kata----make paging action perform in a ajax way.

--- OdeToFood/OdeToFood.Web/Scripts/OdeToFood.js
+++ OdeToFood/OdeToFood.Web/Scripts/OdeToFood.js
@@ -17,9 +17,23 @@
         });
         return false;
     }
+    var ajaxPageListUpdate = function () {
+        var options = {
+            url: $(this).attr("href"),
+            type: "get",
+            data: $("#autoComplete").serialize()//send searchterm value to index method, otherwise, if we have 10 pages search result, then cannot display the next pages of search results.
+        }
+        $.ajax(options).done(function (data) {
+            $("div.RestaurantListPartial").html(data).effect("highlight");
+        });
+        return false;
+    }
     $("#autoComplete").autocomplete(options);
     //$("#ajaxTag").click(ajaxGetResaurantList);//way A: this will only trigger the ajax call when an user clicks the submit button but not the autocomplete way.
     $("form[data-otf-ajax='true']").submit(ajaxGetResaurantList);//better way: bind an event handler when the submit event 
     //is triggered.in this way, ajax search will be called either by clicking the submit button or selecting an autocomplete pop-up item.
 
+    //$(".pagedList a").click(ajaxPageListUpdate);//method1: this method leads to a bug that only 1 out of 2 times clicking the next page will trige a ajax 
+    //request, the other request is a http version, so the page will reload every two time, I don't know why
+    $(".body-content").on("click", ".pagedList a", ajaxPageListUpdate);// method 2: fix the method1 bug.
 });
