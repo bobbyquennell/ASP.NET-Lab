90b1da23c6cdfc1f6a92b09179a0d4656f9b149c
bobby wang <bobbyquennell@gmail.com>
2015-07-10 10:40:33 +1000

    OdeToFood Kata----trigger submit action when a user selects one autocomplete item.

--- OdeToFood/OdeToFood.Web/Scripts/OdeToFood.js
+++ OdeToFood/OdeToFood.Web/Scripts/OdeToFood.js
@@ -2,8 +2,8 @@
     var options = {
         source: $("#autoComplete").attr("data-otf-action"),
         select: function (event, ui) {
-            $(this).val(ui.item.value);
-            //$("form[data-otf-ajax='true']").trigger("submit");
+            $(this).val(ui.item.value);//make sure the selected value will be populated in the input box.
+            $("form[data-otf-ajax='true']").trigger("submit");
         }
     };
     $("#autoComplete").autocomplete(options);
