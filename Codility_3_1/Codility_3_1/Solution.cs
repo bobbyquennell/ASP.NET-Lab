21f08a1e4594213ad223583219bfe0402f2987b7
bobby wang <bobbyquennell@gmail.com>
2015-07-10 17:37:19 +1000

    Codility Lesson 3_1

---
+++ Codility_3_1/Codility_3_1/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_3_1
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int counter = 0;
+            int numberofCarMovingRight = 0;
+            foreach (var car in A)
+            {
+                if (car == 0)//found a car moving right
+                {
+                    numberofCarMovingRight += 1;
+
+                }
+                if (car == 1)//found a car moving left, with pass all the car moving right which is found previously
+                {
+
+
+                    counter += numberofCarMovingRight;
+                    if (counter > 1000000000)
+                    {
+                        return -1;
+                    }
+
+                }
+            }
+            return counter;
+        }
+    }
+}
