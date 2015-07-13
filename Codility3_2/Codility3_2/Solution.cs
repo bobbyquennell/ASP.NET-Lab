5a9e388d0a4a34910d3e8a62a9d85f1e7683c47f
bobby wang <bobbyquennell@gmail.com>
2015-07-13 10:57:38 +1000

    Codility Lesson3_2

---
+++ Codility3_2/Codility3_2/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility3_2
+{
+    public class Solution
+    {
+        public int solution(int A, int B, int K)
+        {
+            //int count = 0;
+            //int[] p = new int[B+1];
+            //p[0] = 1;
+            //for (int i = 1; i <= B; i++)
+            //{
+            //    if (i % K == 0)
+            //    {
+            //        p[i] = p[i - 1] + 1;
+            //    }
+            //    else
+            //    {
+            //        p[i] = p[i - 1];
+            //    }
+            //}
+            //if(A%K == 0)
+            //{
+            //    count = p[B] - p[A] + 1;
+            //}else
+            //    count=  p[B] - p[A];
+            //return count;
+            int count = 0;
+            if(A%K !=0){
+                int remainder = A % K;
+                int StepsToMakeA_DivisibleByK = K - remainder;
+                A = A + StepsToMakeA_DivisibleByK;
+                if (A <= B)
+                    count += 1;
+            }
+            else
+            {
+                count = 1;
+            }
+            int distance = B - A;
+            if (distance >= K)
+            {
+                count += distance / K;
+            }
+            return count;
+        }
+    }
+}
---
+++ Codility3_2/Test/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Codility3_2;
+/*Write a function:
+
+class Solution { public int solution(int A, int B, int K); }
+
+that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+
+{ i : A ≤ i ≤ B, i mod K = 0 }
+
+For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+
+Assume that:
+
+A and B are integers within the range [0..2,000,000,000];
+K is an integer within the range [1..2,000,000,000];
+A ≤ B.
+Complexity:
+
+expected worst-case time complexity is O(1);
+expected worst-case space complexity is O(1).*/
+namespace Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test1()
+        {
+            //arrange
+            var sut = new Solution();
+            int A = 6;
+            int B = 11;
+            int K = 2;
+            
+            //act
+            var result = sut.solution(A, B, K);
+            //assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void test2()
+        {
+            //arrange
+            var sut = new Solution();
+            int A = 1000;
+            int B = 1009;
+            int K = 8;
+
+            //act
+            var result = sut.solution(A, B, K);
+            //assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void test3()
+        {
+            //arrange
+            var sut = new Solution();
+            int A = 11;
+            int B = 345;
+            int K = 17;
+
+            //act
+            var result = sut.solution(A, B, K);
+            //assert
+            Assert.That(result, Is.EqualTo(20));
+        }
+    }
+}
