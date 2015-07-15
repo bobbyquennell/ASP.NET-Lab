e846689f0c4da568ac29d7618dd83fce64fd777f
bobby wang <bobbyquennell@gmail.com>
2015-07-15 14:34:44 +1000

    Sort Algorithm Implementation ----Select Sort Algorithm

---
+++ SortAlgorithm/SortAlgorithm/SelectSortAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithm
+{
+    public class SelectSortAlg
+    {
+        public int[] sort(int[] A)
+        {
+            int N = A.Length;
+            int minIndex = 0;
+            for (int i = 0; i < N; i++)
+            {
+                minIndex = i;
+
+                for (int j = i+1; j < N; j++)
+                {
+                    if (A[minIndex] > A[j])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = A[i];
+                A[i] = A[minIndex];
+                A[minIndex] = temp;
+            }
+            return A;
+        }
+    }
+}
---
+++ SortAlgorithm/SortTest/SortTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SortAlgorithm;
+namespace SortTest
+{
+    [TestFixture]
+    public class SortTest
+    {
+        [Test]
+        public void SelectSort_With2Values()
+        {
+            //arrange
+            int[] arr = new int[] { 5, 2 };
+            var sut = new SelectSortAlg();
+            //act
+            var result = sut.sort(arr);
+            //assert
+            Assert.That(result, Is.EqualTo(new int[]{2,5}));
+        }
+        [Test]
+        public void SelectSort_With3Values()
+        {
+            //arrange
+            int[] arr = new int[] { 5, 2, 8 };
+            var sut = new SelectSortAlg();
+            //act
+            var result = sut.sort(arr);
+            //assert
+            Assert.That(result, Is.EqualTo(new int[] { 2, 5, 8 }));
+        }
+        [Test]
+        public void SelectSort_With6Values()
+        {
+            //arrange
+            int[] arr = new int[] { 5, 2, 8,14,1,16 };
+            var sut = new SelectSortAlg();
+            //act
+            var result = sut.sort(arr);
+            //assert
+            Assert.That(result, Is.EqualTo(new int[] {1, 2, 5, 8,14,16 }));
+        }
+    }
+}
