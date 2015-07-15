6a6661f4f4998b0277b702cf8c561c7f5116b089
bobby wang <bobbyquennell@gmail.com>
2015-07-15 15:42:22 +1000

    Sort Algorithm----Counting Sort Algorithm with Tests

---
+++ SortAlgorithm/SortAlgorithm/CountingSortAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithm
+{
+    public class CountingSortAlg:ISortAlgorithm
+    {
+        int _valueRange;
+        public CountingSortAlg(int valueRange)
+        {
+            _valueRange = valueRange;
+        }
+        public int[] sort(int[] A)
+        {
+           int[] Counter = new int[_valueRange+1];//if values in A[] is in range of {0, 1, ...K}, then we need an array with size of k+1
+           for (int i = 0; i < A.Length; i++)
+			{
+			    Counter[A[i]] +=1;
+			}
+            int p = 0;
+            for (int i = 0; i < Counter.Length; i++)
+			{
+			    for (int j = 0; j < Counter[i]; j++)
+			    {
+			        A[p] = i;
+                    p +=1;
+			    }
+			}
+            return A;
+        }
+    }
+}
---
+++ SortAlgorithm/SortTest/CountingSortTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SortAlgorithm;
+
+
+namespace SortTest
+{
+    [TestFixture]
+    class CountingSortTest
+    {
+        [Test]
+        public void CountingSort_With_2Values()
+        {
+            //arrange
+            int[] arr = new int[] { 5, 2 };
+            CountingSortAlg alg = new CountingSortAlg(5);
+            var sut = new Sorter(alg);
+            //act
+            var result = sut.sort(arr);
+            //assert
+            Assert.That(result, Is.EqualTo(new int[] { 2, 5 }));
+        }
+        [Test]
+        public void CountingSort_With_3Values()
+        {
+            //arrange
+            int[] arr = new int[] { 5, 2,8 };
+            CountingSortAlg alg = new CountingSortAlg(8);
+            var sut = new Sorter(alg);
+            //act
+            var result = sut.sort(arr);
+            //assert
+            Assert.That(result, Is.EqualTo(new int[] { 2, 5, 8 }));
+        }
+        [Test]
+        public void CountingSort_With_6Values()
+        {
+            //arrange
+            int[] arr = new int[] { 5, 2, 8,14,1,16 };
+            CountingSortAlg alg = new CountingSortAlg(16);
+            var sut = new Sorter(alg);
+            //act
+            var result = sut.sort(arr);
+            //assert
+            Assert.That(result, Is.EqualTo(new int[] { 1, 2, 5, 8,14,16 }));
+        }
+    }
+}
