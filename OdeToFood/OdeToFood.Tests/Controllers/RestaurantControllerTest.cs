819bca22ee8ddb5c2edd4dd0f42dbef5b34e7590
bobby wang <bobbyquennell@gmail.com>
2015-07-15 22:15:50 +1000

    OdeToFood Kata---- Restaurant Create Action Unit Test-----update it using Moq to mock the Add<T>() operation.

--- OdeToFood/OdeToFood.Tests/Controllers/RestaurantControllerTest.cs
+++ OdeToFood/OdeToFood.Tests/Controllers/RestaurantControllerTest.cs
@@ -8,6 +8,8 @@ using OdeToFood.Web.Controllers;
 using OdeToFood.Domain.Repositories;
 using System.Web.Mvc;
 using OdeToFood.Web.Models.Bristo;
+using Moq;
+using OdeToFood.Domain.Entities;
 namespace OdeToFood.Tests.Controllers
 {
     [TestFixture]
@@ -17,19 +19,21 @@ namespace OdeToFood.Tests.Controllers
         public void Create_Saves_New_Restaurant_When_Valid()
         {
             //arrange
-            FakeRepo repo = new FakeRepo();
-            var sut = new RestaurantController(repo);
-            RestaurantEditViewModel model = new RestaurantEditViewModel()
-            {
-                City = "Xi'an",
-                Country = "China",
-                Name = "ZhuYuanCun"
-            };
+            //FakeRepo repo = new FakeRepo();
+            List<Restaurant> Restaurants = new List<Restaurant>();
+            Mock<IRepository> mockRepo = new Mock<IRepository>();
+            mockRepo.Setup(repo => repo.Add<Restaurant>(It.IsAny<Restaurant>())).Callback<Restaurant>(new Action<Restaurant>(
+                r => {
+                    Restaurants.Add(r);
+                }));
+            var sut = new RestaurantController(mockRepo.Object);
+            RestaurantEditViewModel model = new RestaurantEditViewModel();
             //act
             ActionResult result = sut.Create(model);
             //assert
-            Assert.That(repo.AddList.Count, Is.EqualTo(1));
+            Assert.That(Restaurants.Count, Is.EqualTo(1));
         }
+
         [Test]
         public void Create_Will_Not_Save_New_Restaurant_When_InValid()
         {
