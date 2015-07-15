5fac960574a270b2de751f57fb98a713c7e52135
bobby wang <bobbyquennell@gmail.com>
2015-07-15 17:14:22 +1000

    Sort Algorithm ------Add Merge Sort Alg stud, need to implement it in future

---
+++ SortAlgorithm/SortAlgorithm/MergeSortAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithm
+{
+    public class MergeSortAlg:ISortAlgorithm
+    {
+        public int[] sort(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SortAlgorithm/SortTest/MergSortTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SortAlgorithm;
+
+namespace SortTest
+{
+    [TestFixture]
+    class MergSortTest
+    {
+        [Test]
+        public void Test1()
+        {
+            //arrange
+            int[] arr = new int[] { 38, 27,43,3,9,82,10 };
+            MergeSortAlg alg = new MergeSortAlg();
+            var sut = new Sorter(alg);
+            //act
+            var result = sut.sort(arr);
+            //assert
+            Assert.That(result, Is.EqualTo(new int[] { 3,9,10,27,38,43,82}));
+        }
+    }
+}
